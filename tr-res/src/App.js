b7135d33d591bd21a916377c61ef7f6dc2762c57
Aman Kumar <brofistaman@gmail.com>
2022-04-26 20:29:40 +0530

    updated app.js

--- tr-res/src/App.js
+++ tr-res/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Home from "./Components/Screens/Home";
 import Booking from "./Components/Screens/Booking";
+import Login from "./Components/Screens/Login";
+import Signup from "./Components/Screens/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Booking from "./Components/Screens/Booking";
 
@@ -10,15 +12,14 @@ function App() {
             <div className="App">
                 <Routes>
                     {/* <Route exact path="/login">{<Login />}</Route> */}
-                    <Route exact path="/home" element={<Home />} />
+                    <Route exact path="/" element={<Home />} />
                     <Route exact path="/booking" element={<Booking />} />
+                    <Route exact path="/login" element={<Login />} />
+                    <Route exact path="/signup" element={<Signup />} />
                 </Routes>
             </div>
         </Router>
     );
 }
-/* <div className="App">
-            <Home />
-            {/* <Booking /> 
-            </div> */
+
 export default App;
