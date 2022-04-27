a98aafb7b724742e479e57d056005a2bdc7f729e
prianshu <priyanshu20106@iiitd.ac.in>
2022-04-28 04:00:20 +0530

    login,Train

--- tr-res/src/App.js
+++ tr-res/src/App.js
@@ -2,23 +2,20 @@ import "./App.css";
 import Home from "./Components/Screens/Home";
 import Booking from "./Components/Screens/Booking";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Passenger from "./Components/Screens/Passenger";
+import Login from "./Components/Screens/Login"
+import Train from "./Components/Screens/Train";
+
 // import Booking from "./Components/Screens/Booking";
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Routes>
-                    {/* <Route exact path="/login">{<Login />}</Route> */}
-                    <Route exact path="/home" element={<Home />} />
-                    <Route exact path="/booking" element={<Booking />} />
-                </Routes>
+              <Train/>
             </div>
         </Router>
     );
 }
-/* <div className="App">
-            <Home />
-            {/* <Booking /> 
-            </div> */
+
 export default App;
